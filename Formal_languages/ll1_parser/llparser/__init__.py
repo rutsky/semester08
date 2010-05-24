7c82de800f49518396ebecbea68da9e5ce869447
Vladimir Rutsky <altsysrq@gmail.com>
2010-05-24 13:13:32 +0400

    Fix FIRST() and FOLLOW()

--- Formal_languages/ll1_parser/llparser/__init__.py
+++ Formal_languages/ll1_parser/llparser/__init__.py
@@ -3,7 +3,8 @@
 
 import sys
 import pygraph
-from itertools import chain
+from itertools import chain, ifilter
+from ordereddict import OrderedDict
 
 __author__ = "Vladimir Rutsky <altsysrq@gmail.com>"
 __copyright__ = "Copyright  2006, Vladimir Rutsky"
@@ -42,21 +43,58 @@ class UnsupportedGrammarException(Exception):
     
 class LeftRecursionException(UnsupportedGrammarException):
     def __init__(self, message):
-        UnsupportedGrammarException(self, message)
+        UnsupportedGrammarException.__init__(self, message)
 
-class Productions(dict):
-    def __init__(self, start_nonterm, productions):
-        dict.__init__(self, productions)
-        self.start_nonterm = start_nonterm
+class InvalidProductionsSyntax(Exception):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return self.message
+
+class Productions(OrderedDict):
+    def check(self):
+        """Check is productions is syntaxically valid."""
+        # TODO: Not all cases handled.
+        lefts = set(self.keys())
+        left_nonterms = list(filter(lambda s: not isinstance(s, NonTerm), lefts))
+        if len(left_nonterms) != 0:
+            ex = "Left part has not non-terminals: "%(", ".join(map(str, left_nonterms)),)
+            raise InvalidProductionsSyntax(ex)
+        rights = set(chain(*list(chain(*list(self.values())))))
+        right_nonsymbs = list(filter(lambda s: not isinstance(s, (Term, NonTerm)) and not s == empty_symbol, rights))
+        if len(right_nonsymbs) != 0:
+            ex = "Right part has something that is not terminal, or non-terminal, or e: %s."%(
+                ", ".join(map(str, right_nonsymbs)),)
+            raise InvalidProductionsSyntax(ex)
+        rights_nonterms = set(filter(lambda s: isinstance(s, NonTerm), rights))
+        only_rights_nonterms = rights_nonterms.difference(lefts)
+        if len(only_rights_nonterms) != 0:
+            ex = "Right part has non-terminals which are not in left part: "
+            ex += ", ".join(map(str, only_rights_nonterms))
+            ex += "."
+            raise InvalidProductionsSyntax(ex)
+        for left, rights in self.iteritems():
+            for right in rights:
+                if len(right) == 0:
+                    ex = "Empty production: %s."%(left,)
+                    raise InvalidProductionsSyntax(ex)
+                if len(right) >= 2 and next(ifilter(lambda s: s == empty_symbol, right), None) is not None:
+                    ex = "Production right part has e and some symbols: %s -> "%(left,)
+                    ex += " ".join(map(str, right))
+                    raise InvalidProductionsSyntax(ex)
+        return True
     
     def symbols(self):
-        return set(chain(self.keys(), chain(*list(chain(*self.values())))))
+        return set(chain(*list(chain(*self.values()))))
     
     def terms(self):
         return filter(lambda symb: isinstance(symb, Term), self.symbols())
         
     def nonterms(self):
-        return filter(lambda symb: isinstance(symb, NonTerm), self.symbols())
+        return self.keys()
+        
+    def start_nonterm(self):
+        return next(iter(self))
     
     def __str__(self):
         result = ""
@@ -85,47 +123,55 @@ class Productions(dict):
             graph.add_edge(edge_from, edge_to)
         return graph
     
-    def _first(self, cur_nonterm, start_nonterm):
-        result = set()
-        for right in self[nonterm]:
-            assert len(right) >= 1
-            symb = right[0] # Production like "N -> " is incorrect.
-            if isinstance(symb, Term):
-                # N -> 't' ... - add 't' to result.
-                result.add(symb)
-            elif symb == empty_symbol:
-                # N -> e - add e to result.
-                assert len(right) == 1 # Production like "N -> e..." is incorrect.
-                result.add(symb)
-            else:
-                # N -> N1
-                assert isinstance(symb, NonTerm)
-                # Look for first nonterminal which will not produce e,
-                # meantime all previous symbols FIRST's are addded to result.
-                for symb in right:
-                    if symb != start_nonterm:
-                        # TODO: Cache.
-                        firsts = list(self._first(symb, start_nonterm))
-                        result.update(firsts)
-                        if empty_symbol not in firsts:
-                            # Reach "N -> [e|'t'] ... [e|'t'] 't'".
-                            break
-                    else:
-                        # Left recursion.
-                        raise LeftRecursionException("Left recursion for nonterminal %s."%(start_nonterm,))
-        return result
+    def _first(self, cur_symbol, start_symbol):
+        if isinstance(cur_symbol, Term):
+            # Terminal.
+            return [cur_symbol]
+        elif cur_symbol == empty_symbol:
+            # Empty symbol.
+            return [cur_symbol]
+        else:
+            assert cur_symbol in self
         
-    def first(self, *nonterms):
-        assert nonterm in self
-        if len(nonterms) == 1:
+            result = set()
+            for right in self[cur_symbol]:
+                assert len(right) >= 1
+                symb = right[0] # Production like "N -> " is incorrect.
+                if isinstance(symb, Term):
+                    # N -> 't' ... - add 't' to result.
+                    result.add(symb)
+                elif symb == empty_symbol:
+                    # N -> e - add e to result.
+                    assert len(right) == 1 # Production like "N -> e..." is incorrect.
+                    result.add(symb)
+                else:
+                    # N -> N1
+                    assert isinstance(symb, NonTerm)
+                    # Look for first nonterminal which will not produce e,
+                    # meantime all previous symbols FIRST's are addded to result.
+                    for symb in right:
+                        if symb != start_symbol:
+                            # TODO: Cache.
+                            firsts = list(self._first(symb, start_symbol))
+                            result.update(firsts)
+                            if empty_symbol not in firsts:
+                                # Reach "N -> [e|'t'] ... [e|'t'] 't'".
+                                break
+                        else:
+                            # Left recursion.
+                            raise LeftRecursionException("Left recursion for nonterminal %s."%(start_symbol,))
+            return result
+        
+    def first(self, *symbols):
+        if len(symbols) == 1:
             # FIRST() for single nonterminal.
-            return self._first(nonterm, nonterm)
-        elif len(nonterms) >= 2:
+            return self._first(symbols[0], symbols[0])
+        elif len(symbols) >= 2:
             # FIRST("N1 N2 .. NN") is union of FIRST(Ni) for i in 1..K, 
             # where K = argmax for k (e not in FIRST(Ni) for all i=1..k).
             result = set()
-            for nonterm in nonterms:
-                firsts = list(self._first(nonterm, nonterm))
+            for symbol in symbols:
+                firsts = list(self._first(symbol, symbol))
                 result.update(firsts)
                 if empty_symbol not in firsts:
                     # Found nonterminal which not produce e.
@@ -134,11 +180,14 @@ class Productions(dict):
     
     def _follow(self, cur_nonterm, start_nonterm):
         result = set()
-        if nonterm == self.start_nonterm:
+        if cur_nonterm == self.start_nonterm():
             # Start nonterm has $ in FOLLOW().
             result.add(word_end)
         # Look all productions for current nonterminal in right part.
         for left, rights in self.iteritems():
+            if left == cur_nonterm:
+                # Skip production "N -> ... N ..." for FOLLOW(N).
+                continue
             for right in rights:
                 for i, symb in enumerate(right):
                     if symb == cur_nonterm:
@@ -146,7 +195,7 @@ class Productions(dict):
                         # occures in right part.
                         if i + 1 < len(right):
                             # N -> ... X S1 ... SK
-                            firsts = set(self.first(right[i + 1:]))
+                            firsts = set(self.first(*right[i + 1:]))
                             firsts_not_e = firsts.difference([empty_symbol])
                         else:
                             # N -> ... X
@@ -158,14 +207,14 @@ class Productions(dict):
                             result.update(firsts_not_e)
                         # TODO: On well composed LL(1) grammar "firsts" can contain
                         # either e or non-e's.
-                        if empty_word in firsts or len(firsts) == 0:
+                        if empty_symbol in firsts or len(firsts) == 0:
                             # Add FOLLOW(N) to FOLLOW(X).
                             if left != start_nonterm:
                                 result.update(self._follow(left, start_nonterm))
                             else:
                                 # Recursion.
-                                raise UnsupportedGrammarException("Recursion in FOLLOW(%s)"%(start_nonterm))
-        assert empty_word not in result
+                                raise UnsupportedGrammarException("Recursion in FOLLOW(%s)."%(start_nonterm))
+        assert empty_symbol not in result
         return result
         
     def follow(self, nonterm):
--- Formal_languages/ll1_parser/main.py
+++ Formal_languages/ll1_parser/main.py
@@ -13,6 +13,7 @@ from llparser import *
 __author__ = "Vladimir Rutsky <altsysrq@gmail.com>"
 __copyright__ = "Copyright  2006, Vladimir Rutsky"
 
+"""
 E = NonTerm('E')
 T = NonTerm('T')
 F = NonTerm('F')
@@ -25,20 +26,49 @@ rbracket = Term(')')
 
 prods = Productions(E, 
     {
-        E: [[E,  add,  T], [T]],
-        T: [[T,  mult,  F], [F]],
-        F: [[lbracket, E,  rbracket],  [var]]
+        E: [[E, add, T], [T]],
+        T: [[T, mult, F], [F]],
+        F: [[lbracket, E, rbracket], [var]]
     })
+"""
+
+E = NonTerm('E')
+E1 = NonTerm('E1')
+T = NonTerm('T')
+T1 = NonTerm('T1')
+F = NonTerm('F')
+
+var = Term('a')
+add = Term('+')
+mult = Term('*')
+lbracket = Term('(')
+rbracket = Term(')')
+
+prods = Productions([ 
+        (E, [[T, E1]]),
+        (E1, [[add, T, E1], [empty_symbol]]),
+        (T, [[F, T1]]),
+        (T1, [[mult, F, T1], [empty_symbol]]),
+        (F, [[lbracket, E,  rbracket],  [var]])
+    ])
 
 def main(args):
     print "Productions:"
     print prods
+    prods.check()
 
-    gvv = gv.readstring(pygraph.readwrite.dot.write(prods.graph()))
-    #for layout in "circo dot fdp neato nop nop1 nop2 twopi".split():
-    for layout in "circo dot fdp neato twopi".split():
-        gv.layout(gvv, layout)
-        gv.render(gvv, 'png', 'output/input_productions_%s.png'%(layout,))
+    if False:
+        gvv = gv.readstring(pygraph.readwrite.dot.write(prods.graph()))
+        #for layout in "circo dot fdp neato nop nop1 nop2 twopi".split():
+        for layout in "circo dot fdp neato twopi".split():
+            gv.layout(gvv, layout)
+            gv.render(gvv, 'png', 'output/input_productions_%s.png'%(layout,))
+    print "Non-terminals: %s."%(', '.join(map(str, prods.nonterms())))
+    print "Terminals: %s."%(', '.join(map(str, prods.terms())))
+    for nonterm in prods.nonterms():
+        print "FIRST(%s) = {  %s  }."%(nonterm, ', '.join(map(str, prods.first(nonterm))))
+    for nonterm in prods.nonterms():
+        print "FOLLOW(%s) = {  %s  }."%(nonterm, ', '.join(map(str, prods.follow(nonterm))))
 
 if __name__ == "__main__":
     main(sys.argv)
