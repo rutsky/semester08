fd46af83522dde4933903b55114a3736e6ba91a8
Vladimir Rutsky <altsysrq@gmail.com>
2010-06-14 05:25:40 +0400

    Move implementation to .CPP

--- Computer_Graphics/HorizonLineAlgorithm/src/viewport.cpp
+++ Computer_Graphics/HorizonLineAlgorithm/src/viewport.cpp
@@ -35,4 +35,187 @@ namespace viewport
     // debug
     //std::cout << "parent 4: " << parent()->parent()->parent()->parent() << "\n";
   }
+  
+  void Viewport::draw()
+  {
+    if (frame_.width() >= 1 && frame_.height() >= 1)
+    {
+      // Prepare frame to drawing.
+      frame_.clear(255);
+      //frame_.fill(details::colors::black());
+      
+      //frame_.putPixel(0, 1, FL_WHITE);
+      
+      std::cout << "draw(" << x() << ", " << y() << ", " << w() << ", " << h() << ");\n"; // debug
+      
+      // Calculate domain.
+      Vector2d const origin(xDomainCenter_ - xDomain_ / 2.0, yDomainCenter_ - yDomain_ / 2.0);
+      Vector2i const extent((int)xCells_ + 1, (int)yCells_ + 1);
+      Vector2d const domain(xDomain_, yDomain_);
+      Vector2d const unit(
+        static_cast<double>(xDomain_) / xCells_, 
+        static_cast<double>(yDomain_) / yCells_);
+      
+      // Build grid.
+      grid::FuncValuesGrid 
+        funcGrid(function::functions[funcIdx_], origin, unit, extent);
+                                  
+      // Build view transformation.
+      // Function in right-handed CS where:
+      //   OX looks right,
+      //   OY looks from us,
+      //   OZ looks up.
+      
+      // Yaw transformation.
+      Eigen::Transform3d const yawTf(
+        Eigen::AngleAxisd(-util::deg2rad(yaw_), Vector3d::UnitZ()));
+      // Pitch transformation.
+      Eigen::Transform3d const pitchTf(
+        Eigen::AngleAxisd(util::deg2rad(pitch_), Vector3d::UnitX()));
+      // Rename axes so
+      //   OX looks right,
+      //   OY looks up,
+      //   OZ looks on us.
+      Eigen::Transform3d const replaceAxesTf(
+        Eigen::AngleAxisd(
+          (Matrix3d() << 
+            1,  0,  0,
+            0,  0,  1,
+            0, -1,  0).finished()));
+      // Move CS center to view volume center.
+      Eigen::Transform3d const viewVolumeTranslateTf(
+        Eigen::Translation3d(
+          static_cast<double>(-xViewVolumeCenter_),
+          static_cast<double>(-yViewVolumeCenter_),
+          0.0));
+      // Scale CS so that X view will contain exactly X domain (similar with Y).
+      Eigen::Transform3d const scaleTf(
+        Eigen::Scaling3d(
+          static_cast<double>(frame_.width() - 1) / xViewVolume_,
+          static_cast<double>(frame_.height() - 1) / yViewVolume_,
+          1.0));
+      // Move CS center to screen center.
+      Eigen::Transform3d const translateTf(
+        Eigen::Translation3d(
+          static_cast<double>(frame_.width()) / 2.0,
+          static_cast<double>(frame_.height()) / 2.0,
+          0.0));
+      
+      Eigen::Transform3d const totalTf = 
+        translateTf * scaleTf * replaceAxesTf * pitchTf * yawTf * viewVolumeTranslateTf;
+      
+      // Sort direction.
+      Vector3d const sortDir(
+        Eigen::AngleAxisd(util::deg2rad(yaw_), Vector3d::UnitZ()) * 
+        Vector3d(0.0, 1.0, 0.0));
+      
+      // Build and sort drawing segments list.
+      typedef edge_gen::EdgesGenerator<edge::edge_t> edges_gen_t;
+      edges_gen_t edgesGen;
+      {         
+        // Add axes.
+        {
+          // OX.
+          Vector3d const pStart(Vector3d(xViewVolumeCenter_, yViewVolumeCenter_, 0.0));
+          Vector3d const pEnd(Vector3d(xViewVolumeCenter_ + xViewVolume_ / 2.0, yViewVolumeCenter_, 0.0));
+          size_t const nFractions = std::max(xCells_ * 2, yCells_ * 2);
+          Vector3d const step = (pEnd - pStart) / static_cast<double>(nFractions);
+          Vector3d p(pStart);
+          color::color_t c = color::red();
+          for (size_t i = 0; i < nFractions; ++i, p += step)
+          {
+            Vector3d const p0(p);
+            Vector3d const p1(p + step);
+            
+            edge::edge_t axis(
+              p0,
+              p1,
+              edge::line_style_t(c, edge::rs_solid), // init
+              edge::line_style_t(c, edge::rs_solid), // above
+              edge::line_style_t(c, edge::rs_dash),  // inside
+              edge::line_style_t(c, edge::rs_solid), // below
+              true, false);
+            edgesGen.addEdge(axis);
+          }
+        }
+        
+        {
+          // OY.
+          Vector3d const pStart(Vector3d(xViewVolumeCenter_, yViewVolumeCenter_, 0.0));
+          Vector3d const pEnd(Vector3d(xViewVolumeCenter_, yViewVolumeCenter_ + yViewVolume_ / 2.0, 0.0));
+          size_t const nFractions = std::max(xCells_ * 2, yCells_ * 2);
+          Vector3d const step = (pEnd - pStart) / static_cast<double>(nFractions);
+          Vector3d p(pStart);
+          color::color_t c = color::green();
+          for (size_t i = 0; i < nFractions; ++i, p += step)
+          {
+            Vector3d const p0(p);
+            Vector3d const p1(p + step);
+            
+            edge::edge_t axis(
+              p0,
+              p1,
+              edge::line_style_t(c, edge::rs_solid), // init
+              edge::line_style_t(c, edge::rs_solid), // above
+              edge::line_style_t(c, edge::rs_dash),  // inside
+              edge::line_style_t(c, edge::rs_solid), // below
+              true, false);
+            edgesGen.addEdge(axis);
+          }
+        }
+        
+        {
+          // OZ.
+          Vector3d const pStart(Vector3d(xViewVolumeCenter_, yViewVolumeCenter_, 0.0));
+          Vector3d const pEnd(Vector3d(xViewVolumeCenter_, yViewVolumeCenter_, (xViewVolume_ + yViewVolume_) / 4.0));
+          size_t const nFractions = std::max(xCells_ * 2, yCells_ * 2);
+          Vector3d const step = (pEnd - pStart) / static_cast<double>(nFractions);
+          Vector3d p(pStart);
+          color::color_t c = color::blue();
+          for (size_t i = 0; i < nFractions; ++i, p += step)
+          {
+            Vector3d const p0(p);
+            Vector3d const p1(p + step);
+            
+            edge::edge_t axis(
+              p0,
+              p1,
+              edge::line_style_t(c, edge::rs_solid), // init
+              edge::line_style_t(c, edge::rs_solid), // above
+              edge::line_style_t(c, edge::rs_dash),  // inside
+              edge::line_style_t(c, edge::rs_solid), // below
+              true, false);
+            edgesGen.addEdge(axis);
+          }
+        }
+        
+        // Add all grid edges.
+        edgesGen.addGridEdges(
+          funcGrid,
+          color::black(),
+          color::bulgarianRose(), 
+          drawXEdges_, drawYEdges_);
+        
+        edgesGen.sort(sortDir);
+      }
+      
+      // Transform all edges.
+      for (edges_gen_t::iterator edgeIt = edgesGen.begin(); 
+          edgeIt != edgesGen.end();
+          ++edgeIt)
+        edgeIt->transform(totalTf);
+      
+      // Draw to frame.
+      {
+        hla::FrameRenderer<render_frame_t> hlaFrameRenderer(frame_);
+        
+        hlaFrameRenderer.drawEdges(edgesGen.begin(), edgesGen.end());
+      }
+      
+      // Flush frame to window.
+      fl_draw_image(frame_.buffer(), x(), y(), 
+                    frame_.width(), frame_.height(), 
+                    frame_.pixelSize(), frame_.lineSize());
+    }
+  }
 }
