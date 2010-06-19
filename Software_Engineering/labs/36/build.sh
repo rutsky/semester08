7c21b9c275661791dd1cc5f1d77877e7b883d742
Vladimir Rutsky <altsysrq@gmail.com>
2010-06-19 20:47:56 +0400

    Temporary build script.

---
+++ Software_Engineering/labs/36/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export MOZSDKDIR=/usr/lib/xulrunner-devel-1.9.1.9
+#export MOZSDKDIR=/home/bob/stuff/firefox/xulrunner-sdk # 32bit SDK :(
+export TOPSRCDIR=`pwd`
+export XULRUNNER_BIN=/usr/bin/xulrunner
+#make crypto rebuild_crypto=1
+#make crypto
+#make clean
+#make xpi
+#make build
+
+#make -C crypto build
+#make crypto-obsolete-build
+make buildall && make onlyxpi
