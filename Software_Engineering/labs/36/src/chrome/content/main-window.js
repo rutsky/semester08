6a14e500555a59370e3fe88694f4e46cc3e21446
Vladimir Rutsky <altsysrq@gmail.com>
2010-06-21 05:27:32 +0400

    Fill encodings lists.

--- Software_Engineering/labs/36/src/chrome/content/main-window.js
+++ Software_Engineering/labs/36/src/chrome/content/main-window.js
@@ -54,9 +54,61 @@ net.sourceforge.enconv = {
 window.addEventListener("load", function(){ net.sourceforge.enconv.onLoad(); }, false);
 */
 
+var enconvIconv = null;
+var encodings = [];
+
 function onLoad()
 {
   document.getElementById("input-text").value = window.arguments[0];
+
+  try
+  {
+    // Obtain IEnconvIconv.
+    enconvIconv = Components.classes["@enconv.sourceforge.net/enconv/iconv"]
+      .getService(Components.interfaces.IEnconvIconv);
+
+    // Get supported encoding list.
+    encodings = enconvIconv.listEncodings().split("\n")
+      .filter(function (x) { return x.length != 0; }).sort()
+    //dump(encodings);
+
+    
+    // Converter "From" encodings list.
+    var fromEncodingList = document.getElementById("from-encoding-list");
+    //fromEncodingList.removeAllItems();
+    while (fromEncodingList.itemCount > 0)
+      fromEncodingList.removeItemAt(0);
+    for (var i = 0; i < encodings.length; i++)
+    {
+      fromEncodingList.appendItem(encodings[i], encodings[i]);
+    }
+
+    // Converter "To" encodings list.
+    var toEncodingList = document.getElementById("to-encoding-list");
+    //toEncodingList.removeAllItems();
+    while (toEncodingList.itemCount > 0)
+      toEncodingList.removeItemAt(0);
+    for (var i = 0; i < encodings.length; i++)
+    {
+      toEncodingList.appendItem(encodings[i], encodings[i]);
+    }
+    
+    // Fill view encodings list.
+    var viewEncodingList = document.getElementById("view-encoding-list");
+    //viewEncodingList.removeAllItems();
+    while (viewEncodingList.itemCount > 0)
+      viewEncodingList.removeItemAt(0);
+    for (var i = 0; i < encodings.length; i++)
+    {
+      viewEncodingList.appendItem(encodings[i], encodings[i]);
+    }
+  }
+  catch (e)
+  {
+    // TODO
+    alert("Encoding Converter failed: " + e);
+    throw e;
+  }
 }
 
 window.addEventListener("load", onLoad, false);
